593b6d378d51c2f706a9ea1f82dab0eefc6468bf
slim <slimabida21@gmail.com>
2025-06-25 11:30:57 +0200

    Traitement du breakpoint du site et gestion du menu-buger avec kavascript avec la fonction setTimeout Test OK

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const typed = new Typed(".multiple", {
   backDelay: 1000,
   loop: true,
 });
+
+// traitement du menu-burger responsive max-width 768px avec un temps d'affichage via le setTimeout //
+const menuBurger = document.getElementById("menu-burger");
+const navigation = document.querySelector(".navigation");
+
+let menuTimeout;
+
+menuBurger.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+
+  // si le menu vient de s'ouvrir on lance le timeout //
+  if (navigation.classList.contains("active")) {
+    clearTimeout(menuTimeout);
+    menuTimeout = setTimeout(() => {
+      navigation.classList.remove("active");
+    }, 2000);
+  } else {
+    clearTimeout(menuTimeout);
+  }
+});
